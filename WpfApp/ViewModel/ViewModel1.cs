c9fd9519d4358435a230c9667695a5381bbe533a
Synless <Synless@users.noreply.github.com>
2018-11-27 11:25:40 +0100

    Add files via upload

---
+++ WpfApp/ViewModel/ViewModel1.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModel
+{
+    class ViewModel1 : INotifyPropertyChanged
+    {
+        private string text1 = "";
+        public string Text1
+        {
+            get
+            {
+                return text1;
+            }
+            set
+            {
+                text1 = value;
+                OnPropertyChanged("Text1");
+            }
+        }
+        private string text2 = "";
+        public string Text2
+        {
+            get
+            {
+                return text2;
+            }
+            set
+            {
+                text2 = value;
+                OnPropertyChanged("Text2");
+            }
+        }
+
+        public ICommand CopyCommand { get; set; }
+
+        public ViewModel1()
+        {
+            CopyCommand = new Command(Copy);
+        }
+        private void Copy(object parametre)
+        {
+            Text2 = Text1;
+        }
+        
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
